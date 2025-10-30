95f052d029803a6c03cbf498c8e7955e1eefcb19
Ranjit-08 <ranjitsamalarj@gmail.com>
2025-10-30 15:44:30 +0530

    day04

--- day04-statelocking/backend.tf
+++ day04-statelocking/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key    = "day04/terraform.tfstate"
     #use_lockfile = true # enabling state locking in latest terraform versions
     region = "us-east-1"
-     dynamodb_table = "lock-file" # any version we can use dynamodb locking
+    dynamodb_table = "lock-file" # any version we can use dynamodb locking
   }
 }
